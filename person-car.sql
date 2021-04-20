881277ede2afe36dc499163192baf4b240b369d7
Achraf Hamid <72417895+achfeed@users.noreply.github.com>
2021-04-21 00:01:35 +0200

    Add files via upload

--- person-car.sql
+++ person-car.sql
@@ -1,13 +1,3 @@
-create table person (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	gender VARCHAR(7) NOT NULL,
-	email VARCHAR(100),
-	date_of_birth DATE NOT NULL,
-	country_of_birth VARCHAR(50) NOT NULL
-);
-
 create table car (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	make VARCHAR(100) NOT NULL,
@@ -15,9 +5,28 @@ create table car (
 	price NUMERIC(19, 2) NOT NULL
 );
 
-insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('Fernanda', 'Beardon', 'Female', 'fernandab@is.gd', '1953-10-28', 'Comoros');
-insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('Omar', 'Colmore', 'Male', null, '1921-04-03', 'Finland');
-insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth) values ('John', 'Matuschek', 'Male', 'john@feedburner.com', '1965-02-28', 'England');
+create table person (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(7) NOT NULL,
+	email VARCHAR(100),
+	date_of_birth DATE NOT NULL,
+	country_of_birth VARCHAR(50) NOT NULL,
+	car_id BIGINT REFERENCES car(id),
+	UNIQUE(car_id)
+);
 
+-- INSERT INTO PERSON
+insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth, car_id)
+values ('Fernanda', 'Beardon', 'Female', 'fernandab@is.gd', '1953-10-28', 'Comoros', '1');
+
+insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth, car_id)
+values ('Omar', 'Colmore', 'Male', null, '1921-04-03', 'Finland', '2');
+
+insert into person (first_name, last_name, gender, email, date_of_birth, country_of_birth, car_id)
+values ('Adriana', 'Matuschek', 'Female', 'amatuschek2@feedburner.com', '1965-02-28', 'Cameroon');
+
+-- INSERT INTO CAR
 insert into car (make, model, price) values ('Land Rover', 'Sterling', '87665.38');
 insert into car (make, model, price) values ('GMC', 'Acadia', '17662.69');
